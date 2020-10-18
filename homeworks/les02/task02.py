39d4e0a98a582ce38f6a96c816a757202116c441
Alex Klyanchin <k142@mail.ru>
2020-10-18 14:14:47 +0300

    les02 task02 release

--- homeworks/les02/task02.py
+++ homeworks/les02/task02.py
@@ -5,3 +5,27 @@
 Для заполнения списка элементов необходимо использовать функцию input().
 
 """
+str_elem = None
+elements = []
+sym_stop = {'', ' ', 'q'}  # стоп символы
+
+while str_elem not in sym_stop:  # вводить элементы до стоп символов
+    str_elem = input('Введите элемент :')
+    if str_elem in sym_stop:  # запретить ввод стоп-символов
+        break
+    elements.append(str_elem)
+
+part_odd = elements[0::2]  # нечетные элементы
+part_even = elements[1::2]  # четные элементы
+list_pair = []
+
+for idx in range(len(part_even)):  # проход по количеству четных элементов
+    # собираем массив [четный, нечетный]
+    list_pair.append(part_even[idx])
+    list_pair.append(part_odd[idx])
+
+# список нечетный - добавить последний элемент из нечетного списка
+if len(elements) % 2:
+    list_pair.append(part_odd[-1])
+
+print(list_pair)
