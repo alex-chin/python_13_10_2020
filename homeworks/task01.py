60d2e360a480c56b70c9b8dd4715a4fb025921b2
Alex Klyanchin <k142@mail.ru>
2020-10-26 00:30:28 +0300

    les04 task01 alpha

--- homeworks/task01.py
+++ homeworks/task01.py
@@ -3,3 +3,13 @@
 В расчете необходимо использовать формулу: (выработка в часах * ставка в час) + премия.
 Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+
+_, param_produce_hours, param_rate, param_bonus = argv
+
+
+def salary(produce_hours, rate, bonus):
+    return produce_hours * rate + bonus
+
+
+print('Зарплата', salary(int(param_produce_hours), int(param_rate), int(param_bonus)))
