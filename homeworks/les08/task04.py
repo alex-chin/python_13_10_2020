16e7a75cc0f9b3e86ec934f545811033424540de
Alex Klyanchin <k142@mail.ru>
2020-11-11 17:55:12 +0300

    les08 task[6] beta

--- homeworks/les08/task04.py
+++ homeworks/les08/task04.py
@@ -2,3 +2,31 @@
 который будет базовым для классов-наследников. Эти классы — конкретные типы оргтехники (принтер, сканер, ксерокс). В
 базовом классе определить параметры, общие для приведенных типов. В классах-наследниках реализовать параметры,
 уникальные для каждого типа оргтехники. """
+
+
+class WarehouseEquipments:
+    shelf = {}
+
+    def __init__(self, shelf):
+        self.shelf = shelf
+
+
+class OfficeEquipment:
+    inventory_number = 0
+    dimensions = (0, 0)
+    weight = 0
+    place = (0, 0)
+    is_network = False
+
+
+class Printer(OfficeEquipment):
+    letter_size = ''
+    color = False
+
+
+class CopyMachine(OfficeEquipment):
+    auto_feeder = False
+
+
+class Scanner(OfficeEquipment):
+    is_pdf_ready = False
